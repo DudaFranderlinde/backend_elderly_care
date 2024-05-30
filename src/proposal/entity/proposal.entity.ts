8bdb7e37a9c3c7196a6c006d2e06b551526f6817
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-05-30 18:21:25 -0300

    Criando entity para propostas

---
+++ src/proposal/entity/proposal.entity.ts
@@ -0,0 +1,22 @@
+import { CaregiverEntity } from "src/caregiver/entities/caregiver.entity";
+import { ElderEntity } from "src/patients/entities/elder.entity";
+import { Status } from "src/utils/enum/proposal-status.enum";
+import { Column, Entity, ManyToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('proposal')
+export class ProposalEntity {
+    @PrimaryGeneratedColumn()
+    id_proposal: number;
+
+    @Column({type: "decimal", nullable: false})
+    valor_hora: number;
+
+    @Column({type: "enum", nullable: false, default: Status.ENVIADO})
+    status: Status;
+
+    @OneToOne(()=> CaregiverEntity, (caregiver)=> caregiver.address)
+    caregiver_id: CaregiverEntity;
+    
+    @OneToOne(()=> ElderEntity, (elder)=> elder.address)
+    elder_id: ElderEntity;
+}
