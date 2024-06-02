e61427f40886c4f1d0cb3aea2a2307edbcf39c33
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-06-02 20:17:12 -0300

    Corrigindo import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './core/auth/guard/jwt-strategy';
 import { CaregiverEntity } from './caregiver/entities/caregiver.entity';
 import { PatientsModule } from './patients/patients.module';
+import { CaregiverModule } from './caregiver/caregiver.module';
+import { databaseProviders } from './core/database/database.providers';
 
 @Module({
   imports: [
@@ -17,7 +19,7 @@ import { PatientsModule } from './patients/patients.module';
         expiresIn: '6h',
       },
     }),
-    CaregiverEntity,
+    CaregiverModule,
     PatientsModule,
   ],
   controllers: [],
