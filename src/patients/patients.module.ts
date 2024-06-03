9592295e44ecd5d91d081dd58d86fc2caff77ddd
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-06-03 18:32:16 -0300

    Importando no módulo

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { patientsProviders } from "./patients.providers";
+import { databaseProviders } from "src/core/database/database.providers";
+import { PatientService } from "./service/patients.service";
+import { PatientsController } from "./controller/patients.controller";
 
 @Module({
     imports: [],
-    controllers: [],
-    providers: [],
+    controllers: [PatientsController],
+    providers: [...patientsProviders, ...databaseProviders, PatientService],
 })
 export class PatientsModule {}
