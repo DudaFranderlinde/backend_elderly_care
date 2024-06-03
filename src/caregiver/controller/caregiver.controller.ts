92124e29c4b02da3712d642132be2aaaabb92861
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-06-03 19:19:24 -0300

    Criando rotas e métodos para ver perfil de cuidador

--- src/caregiver/controller/caregiver.controller.ts
+++ src/caregiver/controller/caregiver.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, HttpException, HttpStatus, Post, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, HttpException, HttpStatus, Post, ValidationPipe, Request, Res } from "@nestjs/common";
 import { CaregiverService } from "../service/caregiver.service";
 import { CreateCaregiverDTO } from "../dto/createCaregiver.dto";
 import { CreateAddressDto } from "src/utils/dto/createAddress.dto";
+import { CaregiverEntity } from "../entities/caregiver.entity";
+import { Response } from 'express';
 
 @Controller('caregiver')
 export class CaregiverController {
@@ -21,4 +23,20 @@ export class CaregiverController {
             message: 'Cadastro realizado.'
           }
     }
+
+    @Get('/profile')
+    async me(@Request() req, @Res() response: Response) {
+      try {
+        const user = await this.service.getProfile(9)
+       
+        if (user) {
+          response.status(HttpStatus.OK).send(user)
+          return user
+        }
+
+        response.status(HttpStatus.BAD_REQUEST).send(`message:{Nenhum usuário encontrado com o ID ${req.user.id}}`)
+      } catch (error) {
+        throw new HttpException({ reason: error?.detail }, HttpStatus.BAD_REQUEST)
+      }      
+    } 
 }
