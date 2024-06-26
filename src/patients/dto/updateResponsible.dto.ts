a2d04c63496d1c3b6cc3ed2a1a4efd622f3f3e2d
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-06-26 19:02:44 -0300

    Criando dtos

---
+++ src/patients/dto/updateResponsible.dto.ts
@@ -0,0 +1,29 @@
+import { Type } from "class-transformer";
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsPhoneNumber, IsString, Matches, ValidateNested } from "class-validator";
+import { UpdateAddressDto } from "src/utils/dto/updateAddress.dto";
+import { Kinship } from "src/utils/enum/kinship-responsible.enum";
+
+export class UpdateResponsibleDto {
+    @IsOptional()
+    @IsString()
+    @IsPhoneNumber('BR')
+    phone: string;
+
+    @IsOptional()
+    @IsEnum(Kinship)
+    kinship: Kinship;
+
+    @IsOptional()
+    @IsString()
+    @IsEmail()
+    email: string;
+    
+    @IsOptional()
+    @IsString()
+    pass: string;
+
+    @ValidateNested()
+    @IsOptional()
+    @Type(()=> UpdateAddressDto)
+    address: UpdateAddressDto;
+}
