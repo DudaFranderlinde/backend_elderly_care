666354728e0fe1b6b48d73f9aee03b0038f7d393
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2024-06-29 00:05:28 -0300

    Alteração de entidades

--- src/patients/entities/responsible.entity.ts
+++ src/patients/entities/responsible.entity.ts
@@ -2,6 +2,7 @@ import { AddressEntity } from "src/utils/entities/address.entity";
 import { Kinship } from "src/utils/enum/kinship-responsible.enum";
 import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { ElderEntity } from "./elder.entity";
+import { ProposalEntity } from "src/proposal/entity/proposal.entity";
 
 @Entity({name: 'responsible'})
 export class ResponsibleEntity {
@@ -20,7 +21,7 @@ export class ResponsibleEntity {
     @Column({type: "enum", enum: Kinship, nullable: false})
     kinship: Kinship;
 
-    @Column({type: "varchar", nullable: false})
+    @Column({type: "varchar", nullable: false, unique: true})
     email: string;
 
     @Column({type: "varchar", nullable: false})
@@ -36,4 +37,8 @@ export class ResponsibleEntity {
     @OneToMany(()=> ElderEntity, (elder)=> elder.responsible_id)
     @JoinColumn({name: 'elder_id'})
     elder_id: ElderEntity;
+
+    @OneToMany(()=> ProposalEntity, (proposal)=> proposal.resposible_id)
+    @JoinColumn({name: 'proposal_id'})
+    proposal_id: ProposalEntity;
 }
--- src/proposal/entity/proposal.entity.ts
+++ src/proposal/entity/proposal.entity.ts
@@ -1,22 +1,22 @@
 import { CaregiverEntity } from "src/caregiver/entities/caregiver.entity";
 import { ElderEntity } from "src/patients/entities/elder.entity";
+import { ResponsibleEntity } from "src/patients/entities/responsible.entity";
 import { Status } from "src/utils/enum/proposal-status.enum";
-import { Column, Entity, ManyToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('proposal')
 export class ProposalEntity {
     @PrimaryGeneratedColumn()
     id_proposal: number;
 
-    @Column({type: "decimal", nullable: false})
-    valor_hora: number;
+    @ManyToOne(()=> CaregiverEntity, (caregiver)=> caregiver.proposal_id)
+    @JoinColumn({name:"caregiver_id"})
+    caregiver_id: CaregiverEntity;
+    
+    @ManyToOne(()=> ResponsibleEntity, (responsible)=> responsible.proposal_id)
+    @JoinColumn({name:"resposible_id"})
+    resposible_id: ResponsibleEntity;
 
     @Column({type: "enum", enum: Status, nullable: false, default: Status.ENVIADO})
     status: Status;
-
-    @OneToOne(()=> CaregiverEntity, (caregiver)=> caregiver.address)
-    caregiver_id: CaregiverEntity;
-    
-    @OneToOne(()=> ElderEntity, (elder)=> elder.address)
-    elder_id: ElderEntity;
 }
